84f6fe714e5bdcbc4cc0b3dc9f0b2b77ef671a65
jrandiny <jrandiny@gmail.com>
2020-09-17 19:06:25 +0700

    Add flatten layer
    Co-Authored-By: a-johanes <antoniusjohanes99@gmail.com>
    Co-Authored-By: SteveImmanuel <steveandreasimanuel@gmail.com>

---
+++ coolcnn/layers/flatten_layer.py
@@ -0,0 +1,11 @@
+from coolcnn.layers.base_layer import BaseLayer
+
+from nptyping import ndarray
+
+
+class Flatten(BaseLayer):
+    def __init__(self, **kwargs):
+        super.__init__(kwargs)
+
+    def process(self, input_layer: ndarray) -> ndarray:
+        return input_layer.flatten()
