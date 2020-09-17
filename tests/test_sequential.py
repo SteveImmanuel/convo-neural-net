16ffb194900b23ca06d23ac993e6b02fbf76e87f
a-johanes <antoniusjohanes99@gmail.com>
2020-09-17 23:43:35 +0700

    Finish sequential test
    Co-Authored-By: jrandiny <jrandiny@gmail.com>
    Co-Authored-By: SteveImmanuel <steveandreasimanuel@gmail.com>

--- tests/test_sequential.py
+++ tests/test_sequential.py
@@ -19,3 +19,81 @@ def test_sequential_1():
 
     model.compile()
     result = model.run(input_layer)
+
+
+def test_sequential_2():
+    input_layer = np.arange(625 * 3).reshape((25, 25, 3)).astype(float)
+
+    model = Sequential(
+        [
+            Convolutional(
+                n_kernel=5, kernel_shape=2, strides=1, padding=1, input_shape=(25, 25, 3)
+            ),
+            Pooling(kernel_shape=(2, 2), strides=2),
+            Convolutional(n_kernel=10, kernel_shape=2, strides=1, padding=1),
+            Pooling(kernel_shape=(2, 2), strides=2),
+            Convolutional(n_kernel=15, kernel_shape=2, strides=1, padding=1),
+            Pooling(kernel_shape=(2, 2), strides=2),
+            Flatten(),
+            Dense(n_nodes=100),
+            Dense(n_nodes=50),
+            Dense(n_nodes=20),
+            Dense(n_nodes=1),
+        ]
+    )
+
+    model.compile()
+    model.summary()
+    result = model.run(input_layer)
+    assert result.shape == (1, )
+
+
+def test_sequential_3():
+    input_layer = np.array(
+        [
+            [[252, 252], [251, 251], [246, 246], [207, 207], [90, 90]],
+            [[250, 250], [242, 242], [236, 236], [144, 144], [41, 41]],
+            [[252, 252], [244, 244], [228, 228], [102, 102], [43, 43]],
+            [[250, 250], [243, 243], [214, 214], [59, 59], [52, 52]],
+            [[248, 248], [243, 243], [201, 201], [44, 44], [54, 54]],
+        ],
+        dtype=np.float64
+    )
+
+    conv_layer_1 = Convolutional(
+        n_kernel=2,
+        kernel_shape=3,
+        strides=1,
+        padding=0,
+        input_shape=input_layer.shape,
+    )
+
+    pool_layer_1 = Pooling(kernel_shape=3, strides=1)
+
+    dense_layer_1 = Dense(n_nodes=2)
+
+    model = Sequential([conv_layer_1, pool_layer_1, Flatten(), dense_layer_1])
+
+    model.compile()
+
+    conv_layer_1._weights = np.array(
+        [
+            [
+                [[1], [0], [-1]],
+                [[1], [0], [-1]],
+                [[1], [0], [-1]],
+            ], [
+                [[-1], [0], [-1]],
+                [[-1], [0], [-1]],
+                [[-1], [0], [-1]],
+            ]
+        ]
+    )
+    conv_layer_1._bias = np.zeros((2, ))
+
+    dense_layer_1._weights = np.array([[0, 2], [0.001, 0]])
+
+    result = model.run(input_layer)
+    assert result.shape == (2, )
+    assert result[0] == Activation.process(ActivationType.SIGMOID, 0)
+    assert result[1] == Activation.process(ActivationType.SIGMOID, 1084 / 1000)
