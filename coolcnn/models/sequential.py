437abd41fa9bab5391eda0a99f2b2e5e131bc35a
a-johanes <antoniusjohanes99@gmail.com>
2020-09-17 23:05:37 +0700

    Add RTE if not compiled
    Co-Authored-By: jrandiny <jrandiny@gmail.com>
    Co-Authored-By: SteveImmanuel <steveandreasimanuel@gmail.com>

--- coolcnn/models/sequential.py
+++ coolcnn/models/sequential.py
@@ -8,17 +8,22 @@ from coolcnn.layers.base_layer import BaseLayer
 class Sequential():
     def __init__(self, layers: List[BaseLayer] = []) -> None:
         self._layers = layers
+        self._compiled = False
 
     def add_layer(self, layer: BaseLayer) -> None:
         self._layers.append(layer)
 
     def compile(self):
+        self._compiled = True
         input_shape = self._layers[0].input_shape
         for layer in self._layers:
             layer.input_shape = input_shape
             input_shape = layer.output_shape
 
     def run(self, input_array: ndarray) -> ndarray:
+        if not self._compiled:
+            raise RuntimeError('Error, please compile model first')
+
         for layer in self._layers:
             input_array = layer.process(input_array)
 
