ad4f279c11a5bcff4ca2b9c4769a07afb80e1de9
jrandiny <jrandiny@gmail.com>
2020-10-07 08:52:12 +0700

    Add backpropagate for flatten layer
    Co-Authored-By: a-johanes <antoniusjohanes99@gmail.com>
    Co-Authored-By: SteveImmanuel <steveandreasimanuel@gmail.com>

--- coolcnn/layers/flatten_layer.py
+++ coolcnn/layers/flatten_layer.py
@@ -13,5 +13,8 @@ class Flatten(BaseLayer):
     def process(self, input_layer: ndarray) -> ndarray:
         return input_layer.flatten()
 
+    def backpropagate(self, input_layer: ndarray, output_layer: ndarray, d_error_d_out: ndarray) -> ndarray:
+        return d_error_d_out.reshape(input_layer.shape)
+
     def _get_output_shape(self) -> Tuple:
         return (np.prod(self._input_shape), )
