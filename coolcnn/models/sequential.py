1db487cee4bcc94e0b6743cc8a434d0e190a57d8
SteveImmanuel <steveandreasimanuel@gmail.com>
2020-10-08 19:58:35 +0700

    Tidy up code, update output format

--- coolcnn/models/sequential.py
+++ coolcnn/models/sequential.py
@@ -6,7 +6,6 @@ import numpy as np
 import pickle
 import time
 
-from datetime import datetime
 from coolcnn.layers.base_layer import BaseLayer
 
 
@@ -41,20 +40,32 @@ class Sequential():
         self.history = []
 
         step = 0
-        mse = 0.
-        acc = 0.
+        mse = 0.0
+        acc = 0.0
 
-        # start_time = time.time()
-        start = datetime.now()
+        print()
+        print('=====Start Training=====')
+        print('Learning rate:', learning_rate)
+        print('Epoch:', epoch)
+        print('Batch size:', mini_batch)
+        print('Momentum:', momentum)
+        print('Total training data:', len(input_array))
+        print('========================')
+        print()
+
+        start = time.time()
+        total_batch = len(input_array) // mini_batch
         while step < (epoch * len(input_array)):
-            # print('Step', step, ':', end=' ')
             print(
+                'Epoch',
                 step // len(input_array) + 1,
+                ':',
                 step % len(input_array) + 1,
                 '/',
                 len(input_array),
                 end='\r'
             )
+
             data_idx = step % len(input_array)
             res = self.run(input_array[data_idx]).copy()
 
@@ -65,28 +76,24 @@ class Sequential():
 
             self._backpropagate(result_array[data_idx])
 
+            #batch end reached
             if (step + 1) % mini_batch == 0:
-                # +1 because of mod and step start with 0
                 for layer in self._layers:
                     layer.update_weight(momentum, learning_rate)
 
+            # epoch end reached
             if (step + 1) % len(input_array) == 0:
-                # print(
-                #     '{}/{} epoch in {:.2f}s'.format(
-                #         (step + 1) // len(input_array), epoch,
-                #         time.time() - start_time
-                #     )
-                # )
-                start_time = time.time()
-                # one epoch
+                print('Epoch', step // len(input_array) + 1)
+                print('Elapsed:', '{:.3f}'.format(time.time()) - start, 's')
+                print(
+                    'MSE:', '{:.4f}'.format(mse / len(input_array)), 
+                    'Acc:', '{:.4f}'.format(acc / len(input_array))
+                )
                 print()
-                print('elapsed', datetime.now() - start)
-                print('mse', mse / len(input_array))
-                print('acc', acc / len(input_array))
                 self.history.append((mse, acc))
-                start = datetime.now()
-                mse = 0.
-                acc = 0.
+                start = time.time()
+                mse = 0.0
+                acc = 0.0
             step += 1
 
     def summary(self):
@@ -121,8 +128,6 @@ class Sequential():
     def _backpropagate(self, target_array: ndarray) -> None:
         predicted_array = self._input_array_list[-1]
         d_error_d_out = -(target_array - predicted_array)
-        # print(predicted_array, target_array)
-        # print('mse', np.sum((target_array - predicted_array)**2))
 
         for layer, input_array, output_array in zip(
             self._layers[::-1], self._input_array_list[-2::-1], self._input_array_list[::-1]
