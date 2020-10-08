Fix momentum calculation

coolcnn/layers/convolutional_layer.py
@@ -12,9 +12,7 @@ class Convolutional(KernelLayer):
         super().__init__(**kwargs)
         self._activator = activator
 
-    def backpropagate(
-            self, input_layer: ndarray, output_layer: ndarray, d_error_d_out: ndarray
-    ) -> ndarray:
+    def backpropagate(self, input_layer: ndarray, output_layer: ndarray, d_error_d_out: ndarray) -> ndarray:
         """
         derr/dnet = dout/dnet * derror/dout
         output_layer = (ft_map_size, n_kernel)
@@ -35,20 +33,13 @@ class Convolutional(KernelLayer):
 
             kernel_w, kernel_h = self._kernel_shape
 
-            d_error_d_in = np.swapaxes(
-                (np.swapaxes(self._weights, 0, -1) * d_error_d_net[output_row, output_col]), 0, -1
-            )
+            d_error_d_in = np.swapaxes((np.swapaxes(self._weights, 0, -1) * d_error_d_net[output_row, output_col]), 0, -1)
 
-            d_error_d_out_prev[anchor_top:anchor_top + kernel_h,
-            anchor_left:anchor_left + kernel_w] += np.sum(d_error_d_in, axis=0)
+            d_error_d_out_prev[anchor_top:anchor_top + kernel_h, anchor_left:anchor_left + kernel_w] += np.sum(d_error_d_in, axis=0)
 
-            receptive_field_extended = np.repeat(
-                receptive_field.reshape((*receptive_field.shape, 1)), self._n_kernel, axis=-1
-            )
+            receptive_field_extended = np.repeat(receptive_field.reshape((*receptive_field.shape, 1)), self._n_kernel, axis=-1)
 
-            self._weights_delta += np.moveaxis(
-                receptive_field_extended * d_error_d_net[output_row, output_col], -1, 0
-            )
+            self._weights_delta += np.moveaxis(receptive_field_extended * d_error_d_net[output_row, output_col], -1, 0)
             self._bias_delta += d_error_d_net[output_row, output_col]
 
         if self._padding == 0:
@@ -58,26 +49,26 @@ class Convolutional(KernelLayer):
 
     def update_weight(self, momentum: float, learning_rate: float) -> None:
         # print('layer convo')
-        # print('delta weight', self._weights_delta)
-        # print('delta bias', self._bias_delta)
-        self._weights = self._weights - learning_rate * self._weights_delta - momentum * self._prev_weights_delta
-        self._prev_weights_delta = self._weights_delta
+        # print('prev delta weight', np.sum(self._prev_weights_delta))
+        # print('delta weight', np.sum(self._weights_delta))
+        current_weights_delta = learning_rate * self._weights_delta + momentum * self._prev_weights_delta
+        self._weights = self._weights - current_weights_delta
+        self._prev_weights_delta = current_weights_delta
         self._weights_delta = np.zeros(self._weights_delta.shape)
 
-        self._bias = self._bias - learning_rate * self._bias_delta - momentum * self._prev_bias_delta
-        self._prev_bias_delta = self._bias_delta
+        current_bias_delta = learning_rate * self._bias_delta + momentum * self._prev_bias_delta
+        self._bias = self._bias - current_bias_delta
+        self._prev_bias_delta = current_bias_delta
         self._bias_delta = np.zeros(self._bias_delta.shape)
 
     def _on_receptive_field(
-            self,
-            receptive_field: ndarray,
-            feature_map: ndarray,
-            output_row: int,
-            output_col: int,
+        self,
+        receptive_field: ndarray,
+        feature_map: ndarray,
+        output_row: int,
+        output_col: int,
     ) -> None:
-        summed_receptive_field = np.sum(
-            self._weights * receptive_field, axis=(1, 2, 3)
-        ) + self._bias
+        summed_receptive_field = np.sum(self._weights * receptive_field, axis=(1, 2, 3)) + self._bias
         return Activation.process(self._activator, summed_receptive_field), output_row, output_col
 
     def _generate_weight(self):
@@ -87,9 +78,7 @@ class Convolutional(KernelLayer):
         total_filter_element = row * col * n_channel
         self._weights = np.array([], dtype=float)
         # self._weights = np.random.normal(0, 0.08, size=(self._n_kernel, row, col, n_channel))
-        self._weights = Activation.init_weight(
-            self._activator, total_filter_element, self._n_kernel
-        ).reshape(self._n_kernel, row, col, n_channel)
+        self._weights = Activation.init_weight(self._activator, total_filter_element, self._n_kernel).reshape(self._n_kernel, row, col, n_channel)
 
         self._weights_delta = np.zeros(self._weights.shape)
         self._prev_weights_delta = np.zeros(self._weights.shape)

coolcnn/layers/dense_layer.py
@@ -23,12 +23,14 @@ class Dense(BaseLayer):
         # print('layer dense')
         # print('delta weight', self._weights_delta)
         # print('delta bias', self._bias_delta)
-        self._weights = self._weights - learning_rate * self._weights_delta - momentum * self._prev_weights_delta
-        self._prev_weights_delta = self._weights_delta
+        current_weight_delta = learning_rate * self._weights_delta + momentum * self._prev_weights_delta
+        self._weights = self._weights - current_weight_delta
+        self._prev_weights_delta = current_weight_delta
         self._weights_delta = np.zeros(self._weights.shape)
 
-        self._bias = self._bias - learning_rate * self._bias_delta - momentum * self._prev_bias_delta
-        self._prev_bias_delta = self._bias_delta
+        current_bias_delta = learning_rate * self._bias_delta + momentum * self._prev_bias_delta
+        self._bias = self._bias - current_bias_delta
+        self._prev_bias_delta = current_bias_delta
         self._bias_delta = 0
 
     def backpropagate(self, input_layer: ndarray, output_layer: ndarray, d_error_d_out: ndarray) -> ndarray:
@@ -40,8 +42,7 @@ class Dense(BaseLayer):
         # bias = (n_nodes, )
 
         d_error_d_net = Activation.differential(self._activator, output_layer) * d_error_d_out
-        d_error_d_weight = np.matmul(d_error_d_net.reshape(d_error_d_net.shape[0], 1),
-                                     input_layer.reshape(1, input_layer.shape[0]))
+        d_error_d_weight = np.matmul(d_error_d_net.reshape(d_error_d_net.shape[0], 1), input_layer.reshape(1, input_layer.shape[0]))
 
         self._weights_delta += d_error_d_weight
         self._bias_delta += np.sum(d_error_d_net)
@@ -51,8 +52,7 @@ class Dense(BaseLayer):
 
     def _generate_weight(self):
         # self._weights = np.random.normal(0, 0.08, size=(self.__n_nodes, self._input_shape[-1]))
-        self._weights = Activation.init_weight(self._activator, self._input_shape[-1], self.__n_nodes).reshape(
-            self.__n_nodes, self._input_shape[-1])
+        self._weights = Activation.init_weight(self._activator, self._input_shape[-1], self.__n_nodes).reshape(self.__n_nodes, self._input_shape[-1])
         self._weights_delta = np.zeros(self._weights.shape)
         self._prev_weights_delta = np.zeros(self._weights.shape)
 
@@ -61,7 +61,7 @@ class Dense(BaseLayer):
         self._prev_bias_delta = 0
 
     def _get_output_shape(self) -> Tuple:
-        return (self.__n_nodes,)
+        return (self.__n_nodes, )
 
     def _get_trainable_params(self) -> int:
         return (self._input_shape[0] + 1) * self.__n_nodes