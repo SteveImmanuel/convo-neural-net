33aa5ac8f73a598ef455d40e13c34e96df35f4a7
SteveImmanuel <steveandreasimanuel@gmail.com>
2020-10-08 13:21:21 +0700

    Reduce child process number

--- coolcnn/worker.py
+++ coolcnn/worker.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from multiprocessing.pool import Pool
 from multiprocessing import cpu_count
 
+
 class Worker:
     __instance = None
 
@@ -10,7 +11,7 @@ class Worker:
             raise Exception('Only allowed 1 instance')
         else:
             Worker.__instance = self
-            self.pool = Pool(cpu_count()-1)
+            self.pool = Pool(4)
 
     def __del__(self):
         self.pool.close()
